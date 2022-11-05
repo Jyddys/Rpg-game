d1947ff25dee79e689d5bbf8bd30c10d62f6ee5f
jyddys1 <justus.sydanmaa@hotmail.com>
2022-11-05 16:45:34 +0200

    basic layout rdy

---
+++ index.js
@@ -0,0 +1,42 @@
+
+const hero = {
+  elementId: "hero",
+  name: "Wizard",
+  avatar: "images/wizard.png",
+  health: "60",
+  diceRoll: [3, 1, 4],
+  diceCount: 3
+}
+
+const monster = {
+  elementId: "monster",
+  name: "Orc",
+  avatar: "images/orc.png",
+  health: "10",
+  diceRoll: [2],
+  diceCount: 1
+}
+
+
+function renderCharacter(data) {
+  const { elementId,name,avatar,health,diceRoll, diceCount } = data
+
+  const diceHtml = diceRoll.map(function(num) {
+    return  `<div class="dice">${num}</div>`
+  }).join('')
+
+  document.getElementById(elementId).innerHTML = 
+  `<div class="character-card">
+      <h4 class="name"> ${name} </h4>
+      <img class="avatar" src="${avatar}"/>
+      <p class="health">health: <b> ${health} </b></p>
+      <div class="dice-container">
+      ${diceHtml}
+      </div>
+  </div>`; 
+}
+
+renderCharacter(hero)
+renderCharacter(monster)
+
+
