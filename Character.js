17ae9d26b50e9fe0e9df954853f9963e130bb3f7
jyddys1 <justus.sydanmaa@hotmail.com>
2022-11-08 14:25:35 +0200

    Attack button starts diceroll

--- Character.js
+++ Character.js
@@ -1,16 +1,21 @@
-import {getDiceRollArray} from './utils.js'
+import {getDiceRollArray, getDicePlaceholderHtml} from './utils.js'
 
 function Character(data) {
   Object.assign(this, data)
+ 
+  this.diceArray = getDicePlaceholderHtml(this.diceCount)
+
 
   this.getDiceHtml = function(diceCount) {
-    return getDiceRollArray(diceCount).map(function(num) {
+    this.currentDiceScore = getDiceRollArray(this.diceCount)
+    this.diceArray = this.currentDiceScore.map(function(num){
       return `<div class="dice">${num}</div>`
     }).join('')
+   
 }
 
   this.getCharacterHtml = function() {
-    const { elementId,name,avatar,health, diceCount } = this;
+    const { elementId,name,avatar,health, diceCount, diceArray } = this;
     let diceHtml = this.getDiceHtml(diceCount)
     
     return `
@@ -19,10 +24,11 @@ function Character(data) {
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
                 <div class="dice-container">
-                    ${diceHtml}
+                    ${diceArray}
                 </div>
             </div>`;
   }
+
 }
 
 export default Character
